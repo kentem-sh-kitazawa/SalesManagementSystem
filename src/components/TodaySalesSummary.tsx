be8486d78d20a18ac658acb6a4a18d35e759c613
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-15 00:03:07 +0900

    本日の販売状況にHTML要素を配置

--- src/components/TodaySalesSummary.tsx
+++ src/components/TodaySalesSummary.tsx
@@ -1,5 +1,17 @@
 const TodaySalesSummary = () => {
-  return <div>TodaySalesSummary</div>;
+  return (
+    <>
+      <p>本日の販売状況</p>
+      <table>
+        <th>商品名</th>
+        <th>販売価格</th>
+        <th>販売数</th>
+        <th>小計</th>
+      </table>
+      <p>売上合計:</p>
+      <p>利益合計:</p>
+    </>
+  );
 };
 
 export default TodaySalesSummary;
