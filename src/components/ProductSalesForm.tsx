258a04ec7965d929f8a80f58d680d10a62dd02bb
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-14 23:48:01 +0900

    販売処理ページにHTML要素を配置

--- src/components/ProductSalesForm.tsx
+++ src/components/ProductSalesForm.tsx
@@ -1,5 +1,48 @@
+import { useNavigate } from "react-router-dom";
+
 const ProductSalesForm = () => {
-  return <div>ProductSalesForm</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <label>
+        商品
+        <select>
+          <option>a</option>
+        </select>
+      </label>
+      <label>
+        販売数
+        <input type="number"></input>
+      </label>
+      <p>販売日</p>
+      <button>追加</button>
+      <p>カート</p>
+      <table>
+        <tr>
+          <th>商品名</th>
+          <th>個数</th>
+        </tr>
+        <tr>
+          <td>aaa</td>
+        </tr>
+      </table>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        販売
+      </button>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        キャンセル
+      </button>
+    </div>
+  );
+  //在庫がなかったときにアラート
 };
 
 export default ProductSalesForm;
