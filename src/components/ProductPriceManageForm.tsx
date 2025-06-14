90da6c684b47f206a66402199a017d847caa7c93
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-14 23:36:47 +0900

    仕入処理ページにHTML要素を配置

--- src/components/ProductPriceManageForm.tsx
+++ src/components/ProductPriceManageForm.tsx
@@ -1,5 +1,45 @@
+import { useNavigate } from "react-router-dom";
+
 const ProductPriceManageForm = () => {
-  return <div>ProductPriceManageForm</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h2>仕入処理</h2>
+      <label>
+        商品
+        <select>
+          <option>a</option>
+        </select>
+      </label>
+      <label>
+        仕入数
+        <input type="number"></input>
+      </label>
+      <label>
+        仕入価格
+        <input type="number"></input>
+      </label>
+      <label>
+        販売価格
+        <input type="number"></input>
+      </label>
+      <p>仕入日</p>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        仕入
+      </button>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        キャンセル
+      </button>
+    </div>
+  );
 };
 
 export default ProductPriceManageForm;
