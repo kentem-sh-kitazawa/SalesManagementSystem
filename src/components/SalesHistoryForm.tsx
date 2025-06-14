874264c6268d7a22ea98e09031dfd76119b48d03
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-14 23:59:51 +0900

    販売集計ページにHTML要素を配置

--- src/components/SalesHistoryForm.tsx
+++ src/components/SalesHistoryForm.tsx
@@ -1,5 +1,28 @@
+import { useNavigate } from "react-router-dom";
+
 const SalesHistoryForm = () => {
-  return <div>SalesHistoryForm</div>;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <table>
+        <th>商品名</th>
+        <th>販売価格</th>
+        <th>販売数</th>
+        <th>小計</th>
+        <th>販売日</th>
+      </table>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        戻る
+      </button>
+      <p>売上合計</p>
+      <p>利益合計</p>
+    </>
+  );
 };
 
 export default SalesHistoryForm;
