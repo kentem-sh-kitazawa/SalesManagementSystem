9b6ca89d8195cc35b48750c9a687c5ed4f1712c4
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-15 21:38:58 +0900

    在庫がないときの処理、カートに追加する処理を追加

--- src/components/ProductSalesForm.tsx
+++ src/components/ProductSalesForm.tsx
@@ -65,56 +65,96 @@ const ProductSalesForm = ({
           const selectProduct = beforeProductsStocks.find(
             (product) => product.id === soldProductIdRef.current
           );
-          setSoldProducts((prev) => {
-            const beforeSoldProductsStocks: SoldProductType[] = [
-              ...soldProducts,
-            ];
-            const selectSoldProduct = beforeSoldProductsStocks.find(
-              (product) => product.id === soldProductIdRef.current
-            );
-            if (selectSoldProduct) {
-              return prev.map((product) =>
-                product.id === selectProduct?.id
-                  ? {
-                      ...product,
-                      soldQuantiry:
-                        product.soldQuantiry +
-                        Number(soldQuantiryRef.current?.value),
-                      subTotal:
-                        Number(soldQuantiryRef.current?.value) *
-                        selectProduct!.salePrice!,
-                      salesDate: getDate(),
-                    }
-                  : product
-              );
-            } else {
-              return [
-                ...prev,
-                {
-                  soldProductName: selectProduct!.productName,
-                  salePrice: selectProduct!.salePrice!,
-                  soldQuantiry: Number(soldQuantiryRef.current?.value),
-                  subTotal:
-                    Number(soldQuantiryRef.current?.value) *
-                    selectProduct!.salePrice!,
-                  salesDate: getDate(),
-                  id: selectProduct!.id,
-                },
+          if (
+            selectProduct!.stockQuantiry == null ||
+            Number(soldQuantiryRef.current?.value) >
+              selectProduct!.stockQuantiry
+          ) {
+            alert("在庫がありません");
+          } else {
+            //販売した商品を追加する処理
+            setSoldProducts((prev) => {
+              const beforeSoldProductsStocks: SoldProductType[] = [
+                ...soldProducts,
               ];
-            }
-          });
-          const updataSelectProducts = beforeProductsStocks.map(
-            (productStock) =>
-              productStock.id === soldProductIdRef.current
-                ? {
-                    ...productStock,
-                    stockQuantiry:
-                      (productStock.stockQuantiry ?? 0) -
-                      Number(soldQuantiryRef.current?.value),
-                  }
-                : productStock
-          );
-          setProductStocks(updataSelectProducts);
+              const selectSoldProduct = beforeSoldProductsStocks.find(
+                (product) => product.id === soldProductIdRef.current
+              );
+              if (selectSoldProduct) {
+                return prev.map((product) =>
+                  product.id === selectProduct?.id
+                    ? {
+                        ...product,
+                        soldQuantiry:
+                          product.soldQuantiry +
+                          Number(soldQuantiryRef.current?.value),
+                        subTotal:
+                          Number(soldQuantiryRef.current?.value) *
+                          selectProduct!.salePrice!,
+                        salesDate: getDate(),
+                      }
+                    : product
+                );
+              } else {
+                return [
+                  ...prev,
+                  {
+                    soldProductName: selectProduct!.productName,
+                    salePrice: selectProduct!.salePrice!,
+                    soldQuantiry: Number(soldQuantiryRef.current?.value),
+                    subTotal:
+                      Number(soldQuantiryRef.current?.value) *
+                      selectProduct!.salePrice!,
+                    salesDate: getDate(),
+                    id: selectProduct!.id,
+                  },
+                ];
+              }
+            });
+            //カートに追加する処理
+            setTodaySoldProducts((prev) => {
+              const selectAddCartProduct = prev.find((product) => {
+                return product.id === soldProductIdRef.current;
+              });
+              if (selectAddCartProduct) {
+                return prev.map((product) => {
+                  return product.id === selectProduct?.id
+                    ? {
+                        ...product,
+                        todaySoldQuantiry:
+                          product.todaySoldQuantiry +
+                          Number(soldQuantiryRef.current?.value ?? 0),
+                      }
+                    : product;
+                });
+              } else {
+                return [
+                  ...prev,
+                  {
+                    id: soldProductIdRef.current!,
+                    productName: selectProduct!.productName,
+                    todaySoldQuantiry: Number(
+                      soldQuantiryRef.current?.value ?? 0
+                    ),
+                  },
+                ];
+              }
+            });
+            //在庫を減らす処理
+            const updataSelectProducts = beforeProductsStocks.map(
+              (productStock) => {
+                return productStock.id === selectProduct?.id
+                  ? {
+                      ...productStock,
+                      stockQuantiry:
+                        (productStock.stockQuantiry ?? 0) -
+                        Number(soldQuantiryRef.current?.value),
+                    }
+                  : productStock;
+              }
+            );
+            setProductStocks(updataSelectProducts);
+          }
         }}
       >
         追加
