85c4ecf6427985fbeb9a7b267566aeec3886f9a6
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-17 11:11:06 +0900

    関数の定義を削除

--- src/components/SalesHistoryForm.tsx
+++ src/components/SalesHistoryForm.tsx
@@ -10,22 +10,6 @@ const SalesHistoryForm = ({
 }: AllProductsType) => {
   const navigate = useNavigate();
 
-  const getSoldTotal = () => {
-    let total = 0;
-    soldProducts.forEach((soldProduct) => {
-      total = soldProduct.salePrice * soldProduct.soldQuantiry + total;
-    });
-    return total;
-  };
-
-  const getPurchaseTotal = () => {
-    let total = 0;
-    productStocks.forEach((productStock) => {
-      total = productStock.purchasePrice ?? 0 + total;
-    });
-    return total;
-  };
-
   return (
     <>
       <table>
