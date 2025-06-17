7224f2d308c120741f9794dbb1ba29b06c7781d2
sh-kitazawa <sh-kitazawa@kentem.co.jp>
2025-06-17 10:55:11 +0900

    当日に売った商品を表示するためのstateと更新処理を追加

--- src/components/TodaySalesSummary.tsx
+++ src/components/TodaySalesSummary.tsx
@@ -1,10 +1,21 @@
 import type { AllProductsType } from "../Types/AllProductsType";
+import { getDate } from "../utils/dateUtils";
+import type { SoldProductType } from "../Types/SoldProductType";
 
 const TodaySalesSummary = ({
   soldProducts,
   productStocks,
   products,
 }: AllProductsType) => {
+  const [todaySoldProducts, setTodaySoldProducts] = useState<SoldProductType[]>(
+    []
+  );
+  useEffect(() => {
+    const uodataTodaySoldProducts = soldProducts.filter(
+      (soldProduct) => soldProduct.salesDate === getDate()
+    );
+    setTodaySoldProducts(uodataTodaySoldProducts);
+  }, [soldProducts]);
   return (
     <>
       <p>本日の販売状況</p>
